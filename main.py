460700eddb498c350e5ad49193b984c7259a15ba
darshanquary <gadgedarshan26@gmail.com>
2026-07-23 16:41:18 +0530

    Added streamlit to requirements

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ def index():
     return render_template('index.html')
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
